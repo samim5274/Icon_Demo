8a7be05696a5da22580b5fc604127c121c76c757
Samim Hossen <banglarcele1122@gmail.com>
2022-03-18 12:34:31 +0600

    up

---
+++ webAppTest/Web/Web/DemoDormitory/DemoDormitory/Models/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DemoDormitory.Models
+{
+    public class AppDbContext : DbContext 
+    {
+        public DbSet<StudentInfo> StudentInfos { get; set; }
+        public AppDbContext() : 
+            base("name=connStr")
+        {
+
+        }
+    }
+}
